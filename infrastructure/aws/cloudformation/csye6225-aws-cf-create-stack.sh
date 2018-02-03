c5b6d77de0f39ed0a206454e55f4357bb6c0ac7a
Zenan <lukeeechen@gmail.com>
2018-02-03 00:33:59 -0500

    fix

--- infrastructure/aws/cloudformation/csye6225-aws-cf-create-stack.sh
+++ infrastructure/aws/cloudformation/csye6225-aws-cf-create-stack.sh
@@ -1,3 +1,2 @@
 #author: chen.zena@husky.neu.edu
-aws cloudformation create-stack  --stack-name startmyinstance  
-    --template-body file://csye6225-cf-networking.json
+aws cloudformation create-stack  --stack-name startmyinstance  --template-body file://csye6225-cf-networking.json
