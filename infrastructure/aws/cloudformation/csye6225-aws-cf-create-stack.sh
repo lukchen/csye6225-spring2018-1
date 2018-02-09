d3c5271d34a7341a7ea7ba9d9e5d900ee798f824
Zenan <lukeeechen@gmail.com>
2018-02-09 13:59:08 -0500

    1

--- infrastructure/aws/cloudformation/csye6225-aws-cf-create-stack.sh
+++ infrastructure/aws/cloudformation/csye6225-aws-cf-create-stack.sh
@@ -1,3 +1,2 @@
 #author: chen.zena@husky.neu.edu
-aws ec2 create-key-pair --key-name STACK_NAME
 aws cloudformation create-stack --stack-name STACK_NAME-csye6225-vpc --template-body file://csye6225-cf-networking.json
