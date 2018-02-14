30707e2fa9e18cbc9e3b629c4bc436fac4b3aa8d
Zenan <lukeeechen@gmail.com>
2018-02-14 14:13:27 -0500

    123

--- infrastructure/aws/cloudformation/csye6225-aws-cf-create-application-stack.sh
+++ infrastructure/aws/cloudformation/csye6225-aws-cf-create-application-stack.sh
@@ -1,1 +1,11 @@
-aws cloudformation create-stack --stack-name assignment4stack --template-body file://csye6225-cf-application.json --parameters ParameterKey=STACKNAME,ParameterValue="STACK_NAME"
+aws cloudformation create-stack \
+
+--stack-name assignment4stack \
+
+--template-body file://csye6225-cf-application.json \
+
+--parameters ParameterKey=STACKNAME,ParameterValue="STACK_NAME" \
+ParameterKey=InstanceType,ParameterValue=t2.micro \
+ParameterKey=ImageId,ParameterValue=ami-66506c1c \
+ParameterKey=Size,ParameterValue=16 \
+ParameterKey=VolumeType,ParameterValue=gp2 
