a94f666965172e660de3a1dafcc0c60931fdf94e
Zenan <lukeeechen@gmail.com>
2018-02-14 11:07:14 -0500

    1

--- infrastructure/aws/cloudformation/csye6225-aws-cf-create-stack.sh
+++ infrastructure/aws/cloudformation/csye6225-aws-cf-create-stack.sh
@@ -1,2 +1,2 @@
 #author: chen.zena@husky.neu.edu
-aws cloudformation create-stack  --stack-name csye6225vpc --template-body file://csye6225-cf-networking.json
+aws cloudformation create-stack --stack-name assignment4stack --template-body file://csye6225-cf-application.json --parameters ParameterKey=STACKNAME,ParameterValue="STACK_NAME"
