161a7c96d1457766a372b0daa904492736277e78
Zenan <lukeeechen@gmail.com>
2018-02-14 13:41:55 -0500

    123

--- infrastructure/aws/cloudformation/csye6225-aws-cf-terminate-application-stack.sh
+++ infrastructure/aws/cloudformation/csye6225-aws-cf-terminate-application-stack.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 #author: chen.zena@husky.neu.edu
 EC2_ID=$(aws ec2 describe-instances --filter "Name=tag:Name,Values=MyTag" --query 'Reservations[*].Instances[*].{id:InstanceId}' --output text)
+echo $EC2_ID
 aws ec2 modify-instance-attribute --instance-id $EC2_ID --no-disable-api-termination 
-aws cloudformation delete-stack  --stack-name assignment4stack  
+aws cloudformation delete-stack  --stack-name assignment4stack
