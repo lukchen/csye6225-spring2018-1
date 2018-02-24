c449b63053c42c960f5fae25030fc8000eca2d59
Xinglong <jia.xi@husky.neu.edu>
2018-02-24 16:16:03 -0500

    stack ssh

--- infrastructure/aws/cloudformation/csye6225-aws-cf-terminate-stack.sh
+++ infrastructure/aws/cloudformation/csye6225-aws-cf-terminate-stack.sh
@@ -2,10 +2,19 @@ echo "Below is the stacks you have:"
 aws cloudformation describe-stacks
 read -p "Please enter the Stack name you want to delete: " stack
 
-echo "Start to change instance termination in Stack $stack... "
-EC2_ID=$(aws ec2 describe-instances --filter "Name=tag:aws:cloudformation:stack-name,Values=$stack" --query 'Reservations[*].Instances[*].{id:InstanceId}' --output text)
+echo "Start to change instance termination in Stack $stack ...... "
+EC2_ID=$(aws ec2 describe-instances --filter "Name=tag:Name,Values=MyTag" --query 'Reservations[*].Instances[*].{id:InstanceId}' --output text)
 aws ec2 modify-instance-attribute --instance-id $EC2_ID --no-disable-api-termination 
 
-echo "Start to delete Stack $stack..."
 aws cloudformation delete-stack  --stack-name $stack  
-echo "Stack $stack is deleted."
+
+status=$(aws cloudformation describe-stacks --stack-name $stack --query 'Stacks[*].StackStatus' --output text)
+
+while [ $status != "DELETE_COMPLETE" ]
+	do 
+	echo "Deleting Stack $stack, current status is $status ......"
+	sleep 30
+	status=$(aws cloudformation describe-stacks --stack-name $stack --query 'Stacks[*].StackStatus[]' --output text)
+	done
+
+echo "------Stack $stack is deleted !------"
