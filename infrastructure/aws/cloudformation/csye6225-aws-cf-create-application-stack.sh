0f70688be83e088f769551e5105145a061481332
Zenan <lukeeechen@gmail.com>
2018-02-24 10:46:32 -0500

    assignment6 updated 1

--- infrastructure/aws/cloudformation/csye6225-aws-cf-create-application-stack.sh
+++ infrastructure/aws/cloudformation/csye6225-aws-cf-create-application-stack.sh
@@ -6,3 +6,17 @@ ParameterKey=InstanceType,ParameterValue=t2.micro \
 ParameterKey=ImageId,ParameterValue=ami-66506c1c \
 ParameterKey=Size,ParameterValue=16 \
 ParameterKey=VolumeType,ParameterValue=gp2 
+
+sleep 30s
+
+EC2_ID=$(aws ec2 describe-instances --filter "Name=tag:Name,Values=MyTag" \
+--query 'Reservations[*].Instances[*].{id:InstanceId}' --output text)
+echo $EC2_ID
+
+ProfileName=$(aws iam list-instance-profiles-for-role --role-name CodeDeployEC2ServiceRole \
+--query 'InstanceProfiles[*].InstanceProfileName' --output text)
+echo $ProfileName
+
+aws ec2 associate-iam-instance-profile --instance-id $EC2_ID \
+--iam-instance-profile Name=$ProfileName
+aws ec2 describe-iam-instance-profile-associations
--- infrastructure/aws/cloudformation/csye6225-aws-cf-terminate-ci-cd-stack.sh
+++ infrastructure/aws/cloudformation/csye6225-aws-cf-terminate-ci-cd-stack.sh
@@ -2,16 +2,18 @@ EC2_ID=$(aws ec2 describe-instances --filter "Name=tag:Name,Values=MyTag" \
 --query 'Reservations[*].Instances[*].{id:InstanceId}' --output text)
 echo $EC2_ID
 
-ProfileName=$(aws iam list-instance-profiles-for-role --role-name ec2Role \
+ProfileName=$(aws iam list-instance-profiles-for-role --role-name CodeDeployEC2ServiceRole \
 --query 'InstanceProfiles[*].InstanceProfileName' --output text)
 echo $ProfileName
 
 AssociationId=$(aws ec2 describe-iam-instance-profile-associations \
 --query 'IamInstanceProfileAssociations[*].AssociationId' --output text)
+echo $AssociationId
+
 aws ec2 disassociate-iam-instance-profile --association-id $AssociationId
 
-aws iam remove-role-from-instance-profile --instance-profile-name $ProfileName --role-name ec2Role
+aws iam remove-role-from-instance-profile --instance-profile-name $ProfileName --role-name CodeDeployEC2ServiceRole
 aws iam delete-instance-profile --instance-profile-name $ProfileName
-aws iam delete-role-policy --role-name ec2Role --policy-name ec2policy
-aws iam delete-role --role-name ec2Role
-aws cloudformation delete-stack  --stack-name rolestack  
+aws iam delete-role-policy --role-name CodeDeployEC2ServiceRole --policy-name CodeDeployEC2S3
+aws iam delete-role --role-name CodeDeployEC2ServiceRole
+aws cloudformation delete-stack  --stack-name rolestack 
