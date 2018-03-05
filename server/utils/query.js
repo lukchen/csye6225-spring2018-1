ca114d0ffa3608545949345f1ee957d75ccdfd25
Zenan <lukeeechen@gmail.com>
2018-03-04 21:49:20 -0500

    321

--- server/utils/query.js
+++ server/utils/query.js
@@ -8,6 +8,26 @@ import fs from 'fs'
 import path from 'path'
 import {db, dbName} from '../config/'
 
+var mysql = require('mysql');
+
+var connection = mysql.createConnection({
+  host     : process.env.csye6225-spring2018.chibl3b1ztao.us-east-1.rds.amazonaws.com,
+  user     : process.env.csye6225master,
+  password : process.env.csye6225,
+  port     : process.env.3306
+});
+
+connection.connect(function(err) {
+  if (err) {
+    console.error('Database connection failed: ' + err.stack);
+    return;
+  }
+
+  console.log('Connected to database.');
+});
+
+connection.end();
+
 let pool
 const sqlSource = fs.readFileSync(path.resolve(__dirname, '..', './sql/ashen_db.sql'), 'utf-8')
 const init = mysql.createConnection(db)
