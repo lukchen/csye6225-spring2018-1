96bee152f5ced8f55e7cca3d01c40fb3c42b4b3a
Zenan <lukeeechen@gmail.com>
2018-03-05 21:22:01 -0500

    123321

--- scripts/npminstall.sh
+++ scripts/npminstall.sh
@@ -1,7 +1,3 @@
 cd /home/ubuntu/
 sudo npm install
 sudo npm install pm2
-sudo apt-get install apache2
-sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password password root'
-sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password_again password root'
-sudo apt-get -y install mysql-server
