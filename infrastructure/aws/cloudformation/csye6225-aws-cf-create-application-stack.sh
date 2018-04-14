83a2d77aec54a63a145ddc81777eb26c90e3e0fe
Xinglong <jia.xi@husky.neu.edu>
2018-04-14 12:47:48 -0400

    autooooo scale

--- infrastructure/aws/cloudformation/csye6225-aws-cf-create-application-stack.sh
+++ infrastructure/aws/cloudformation/csye6225-aws-cf-create-application-stack.sh
@@ -1,13 +1,18 @@
 read -p "Please enter the stack name you want to create: " stack
 
-publicSubnetId=$(aws ec2 describe-subnets --filter "Name=tag:Name,Values=public" --query 'Subnets[*].SubnetId' --output text)
+publicSubnetId1=$(aws ec2 describe-subnets --filter "Name=tag:Name,Values=public1" --query 'Subnets[*].SubnetId' --output text)
+publicSubnetId2=$(aws ec2 describe-subnets --filter "Name=tag:Name,Values=public2" --query 'Subnets[*].SubnetId' --output text)
 privateSubnetId1=$(aws ec2 describe-subnets --filter "Name=tag:Name,Values=private1" --query 'Subnets[*].SubnetId' --output text)
 privateSubnetId2=$(aws ec2 describe-subnets --filter "Name=tag:Name,Values=private2" --query 'Subnets[*].SubnetId' --output text)
-ec2SGId=$(aws ec2 describe-security-groups --filter "Name=group-name,Values=csye6225-webapp" --query 'SecurityGroups[*].GroupId' --output text)
-RDSSG=$(aws ec2 describe-security-groups --filter "Name=group-name,Values=csye6225-rds" --query 'SecurityGroups[*].GroupId' --output text)
+#ec2SGId=$(aws ec2 describe-security-groups --filter "Name=group-name,Values=csye6225-webapp" --query 'SecurityGroups[*].GroupId' --output text)
+#RDSSG=$(aws ec2 describe-security-groups --filter "Name=group-name,Values=csye6225-rds" --query 'SecurityGroups[*].GroupId' --output text)
 VpcId=$(aws ec2 describe-vpcs --filter "Name=tag:Name,Values=STACK_NAME-csye6225-vpc" --query 'Vpcs[*].VpcId' --output text)
 Lambdaarn=$(aws lambda get-function --function-name lambdatest --query 'Configuration.FunctionArn' --output text)
+ProfileName=$(aws iam list-instance-profiles-for-role --role-name CodeDeployEC2ServiceRole \
+--query 'InstanceProfiles[*].InstanceProfileName' --output text)
+
 echo "Start to create Stack $stack ......"
+
 aws cloudformation create-stack --stack-name $stack \
 --capabilities CAPABILITY_NAMED_IAM \
 --template-body file://csye6225-cf-application.json --region us-east-1 \
@@ -16,23 +21,22 @@ ParameterKey=InstanceType,ParameterValue=t2.micro \
 ParameterKey=ImageId,ParameterValue=ami-66506c1c \
 ParameterKey=Size,ParameterValue=16 \
 ParameterKey=VolumeType,ParameterValue=gp2 \
-ParameterKey=PublicSubnetId,ParameterValue=$publicSubnetId \
+ParameterKey=PublicSubnetId1,ParameterValue=$publicSubnetId1 \
+ParameterKey=PublicSubnetId2,ParameterValue=$publicSubnetId2 \
 ParameterKey=csye6225vpc,ParameterValue=$VpcId \
-ParameterKey=ec2SG,ParameterValue=$ec2SGId \
-ParameterKey=RDSSG,ParameterValue=$RDSSG \
 ParameterKey=PrivateSubnetId1,ParameterValue=$privateSubnetId1 \
 ParameterKey=PrivateSubnetId2,ParameterValue=$privateSubnetId2 \
-ParameterKey=lambdaarn,ParameterValue=$Lambdaarn
+ParameterKey=lambdaarn,ParameterValue=$Lambdaarn \
+ParameterKey=EC2Profile,ParameterValue=$ProfileName
 
 sleep 60
 EC2_ID=$(aws ec2 describe-instances --filter "Name=tag:Name,Values=MyTag" --query 'Reservations[*].Instances[*].{id:InstanceId}' --output text)
-# echo $EC2_ID
+echo $EC2_ID
 
-ProfileName=$(aws iam list-instance-profiles-for-role --role-name CodeDeployEC2ServiceRole \
---query 'InstanceProfiles[*].InstanceProfileName' --output text)
+
 # echo $ProfileName
 
-aws ec2 associate-iam-instance-profile --instance-id $EC2_ID --iam-instance-profile Name=$ProfileName
+# aws ec2 associate-iam-instance-profile --instance-id $EC2_ID --iam-instance-profile Name=$ProfileName
 
 # aws ec2 describe-iam-instance-profile-associations
 
