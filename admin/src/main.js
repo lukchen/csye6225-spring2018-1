8f6a1659ac62154af11dbf5d1e4a362e49e842f7
Zenan <lukeeechen@gmail.com>
2018-04-19 02:27:59 -0400

    23

--- admin/src/main.js
+++ admin/src/main.js
@@ -13,7 +13,7 @@ import store from './store'
 
 Vue.config.productionTip = false
 
-axios.defaults.baseURL = 'http://csye6225-spring2018-zenan.me:3000'
+axios.defaults.baseURL = 'https://csye6225-spring2018-zenan.me:3000'
 axios.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded'
 
 /* eslint-disable no-new */
